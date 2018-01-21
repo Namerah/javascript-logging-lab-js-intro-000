180bceeb2006d45d438eb61d65be99aaeb4f9cd8
Namerah Farooq <namerahfarooq76@gmail.com>
2018-01-21 08:01:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-console.error()
-console.log()
-console.warn()
+console.log("Hello user Welcome to our ATM")
+console.warn("Dont enter the incorrect atm num again and again ")
+console.error("Sorry We broke our connection with you!")
+console.error("Try again later!")
+
